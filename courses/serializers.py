1caaf1361c73cadba8966849f35c308e261b47b7
YevgS <onegin2003@mail.ru>
2023-06-17 20:07:21 +0600

    'MiniPC'

---
+++ courses/serializers.py
@@ -0,0 +1,10 @@
+# This is our serializer page
+from rest_framework import serializers
+from .models import Course
+
+
+# class CourseSerializer(serializers.ModelSerializer):
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('id', 'url', 'name', 'language', 'price')
